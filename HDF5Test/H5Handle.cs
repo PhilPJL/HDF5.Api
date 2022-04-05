0b4980fca636990a7f3145ae1265f4e0ae371363
Philip Lee <github@pjlconsultants.co.uk>
2022-04-05 18:52:22 +0100

    Tweak

--- HDF5Test/H5Handle.cs
+++ HDF5Test/H5Handle.cs
@@ -4,122 +4,53 @@ using Handle = System.Int64;
 
 namespace HDF5Test
 {
-    public class H5PropertyListHandle : H5Handle
-    {
-        public H5PropertyListHandle(Handle handle) : base(handle)
-        {
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && Handle > 0)
-            {
-                int err = H5P.close(Handle);
-                ClearHandle();
-                AssertError(err);
-            }
-        }
-    }
-
-    public class H5TypeHandle : H5Handle
-    {
-        public H5TypeHandle(Handle handle) : base(handle)
-        {
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && Handle > 0)
-            {
-                int err = H5T.close(Handle);
-                ClearHandle();
-                AssertError(err);
-            }
-        }
-    }
-
-    public class H5FileHandle : H5Handle, IH5Location
-    {
-        public H5FileHandle(Handle handle) : base(handle)
-        {
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && Handle > 0)
-            {
-                int err = H5F.close(Handle);
-                ClearHandle();
-                AssertError(err);
-            }
-        }
-    }
-
-    public class H5GroupHandle : H5Handle, IH5Location
-    {
-        public H5GroupHandle(Handle handle) : base(handle)
-        {
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && Handle > 0)
-            {
-                int err = H5G.close(Handle);
-                ClearHandle();
-                AssertError(err);
-            }
-        }
-    }
-
     public interface IH5Location
     {
         public Handle Handle { get; }
     }
 
+    public class H5PropertyListHandle : H5Handle
+    {
+        public H5PropertyListHandle(Handle handle) : base(handle, H5P.close) { }
+    }
+
+    public class H5TypeHandle : H5Handle
+    {
+        public H5TypeHandle(Handle handle) : base(handle, H5T.close) { }
+    }
+
+    public class H5FileHandle : H5Handle, IH5Location
+    {
+        public H5FileHandle(Handle handle) : base(handle, H5F.close) { }
+    }
+
+    public class H5GroupHandle : H5Handle, IH5Location
+    {
+        public H5GroupHandle(Handle handle) : base(handle, H5G.close) { }
+    }
+
     public class H5DataSetHandle : H5Handle, IH5Location
     {
-        public H5DataSetHandle(Handle handle) : base(handle)
-        {
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && Handle > 0)
-            {
-                int err = H5D.close(Handle);
-                ClearHandle();
-                AssertError(err);
-            }
-        }
+        public H5DataSetHandle(Handle handle) : base(handle, H5D.close) { }
     }
 
     public class H5SpaceHandle : H5Handle
     {
-        public H5SpaceHandle(Handle handle) : base(handle)
-        {
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && Handle > 0)
-            {
-                int err = H5S.close(Handle);
-                ClearHandle();
-                AssertError(err);
-            }
-        }
+        public H5SpaceHandle(Handle handle) : base(handle, H5S.close) { }
     }
 
     public abstract class H5Handle : Disposable
     {
         public Handle Handle { get; protected set; }
+        private Func<Handle, int> Closer { get; }
+
         public const Handle InvalidHandle = -1;
 
-        protected H5Handle(Handle handle)
+        protected H5Handle(Handle handle, Func<Handle, int> closer)
         {
             AssertHandle(handle);
             Handle = handle;
+            Closer = closer;
         }
 
         public static void AssertHandle(Handle handle)
@@ -147,6 +78,16 @@ namespace HDF5Test
         {
             AssertHandle(h.Handle);
             return h.Handle;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && Handle > 0)
+            {
+                int err = Closer(Handle);
+                ClearHandle();
+                AssertError(err);
+            }
         }
     }
 }
