43c62311cf6e63818068c41982352dcd92aeabff
Philip Lee <github@pjlconsultants.co.uk>
2022-04-08 18:18:31 +0100

    Two data sets

--- HDF5Test/CreateFileTestRealData2.cs
+++ HDF5Test/CreateFileTestRealData2.cs
@@ -19,27 +19,45 @@ namespace HDF5Test
             using var file = H5File.Create(@"test-realdata.h5", H5F.ACC_TRUNC);
             Console.WriteLine($"Created file: {file}");
 
+            long measurementId = 12019;
+            using var group = file.CreateGroup($"Measurement:{measurementId}");
+
+            using var rawRecordWriter = H5DataSetWriter.CreateOneDimensionalDataSetWriter<RawRecordHelper.SRawRecord>(group, "RawRecords", RawRecordHelper.CreateH5Type);
+            using var intervalRecordWriter = H5DataSetWriter.CreateOneDimensionalDataSetWriter<IntervalRecordHelper.SIntervalRecord>(group, "IntervalRecords", RawRecordHelper.CreateH5Type);
+            using var context = new TvlAltContext();
+
             Stopwatch s = new Stopwatch();
             s.Start();
 
-            using var writer = H5DataSetWriter.CreateOneDimensionalDataSetWriter<RawRecordHelper.SRawRecord>(file, "RawRecords", RawRecordHelper.CreateH5Type);
-            using var context = new TvlAltContext();
+            // TODO: async queryable/cancellable
+            // TODO: overlap?
 
-            long measurementId = 12019;
-
-            // complicated query to flatten the explicit one-many but implied one-one relationships
-            // so we end up with inner joins
             context
                 .RawRecords
                 .Where(r => r.MeasurementId == measurementId)
                 .Buffer(20)
                 .ForEach(rg =>
                 {
-                    writer.Write(rg.Select(r => RawRecordHelper.Convert(r)));
+                    rawRecordWriter.Write(rg.Select(r => RawRecordHelper.Convert(r)));
                 });
 
             s.Stop();
-            Console.WriteLine($"Time elapsed: {s.Elapsed}. Rows written = {writer.CurrentPosition}");
+            Console.WriteLine($"Time elapsed: {s.Elapsed}. RawRecord rows written = {rawRecordWriter.CurrentPosition}");
+
+            s.Start();
+
+            // TODO: simplify query
+            context
+                .IntervalRecords
+                .Where(r => r.RawRecords.Any(rr => rr.MeasurementId == measurementId))
+                .Buffer(20)
+                .ForEach(rg =>
+                {
+                    intervalRecordWriter.Write(rg.Select(r => IntervalRecordHelper.Convert(r)));
+                });
+
+            s.Stop();
+            Console.WriteLine($"Time elapsed: {s.Elapsed}. IntervalRecord rows written = {intervalRecordWriter.CurrentPosition}");
         }
     }
 }
